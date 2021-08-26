1319799e49b91ca50f665e00ee030492c8882774
JuchokJuk <juk.juchok@yandex.ru>
2021-08-26 13:14:24 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 0, name: 'Крутыш', img: 'https://sun9-78.userapi.com/impg/WdJjnCcz2A5NrPNMD4usp_LUhmNOyTtLEMDrmQ/npfgxU3RoSc.jpg?size=244x244&quality=96&sign=12fab2ba61803449022d612f02224529&type=album' },
+  { id: 1, name: 'Бомбастер', img: 'https://sun9-65.userapi.com/impg/UNJ1Sj0dm1rawgVPvowzRAX9SNV_Y0Kb-ycy3g/u7PPDBmfdNk.jpg?size=1000x667&quality=96&sign=46a6ffd90fbb40c7f591718a079d7385&type=album' },
+  { id: 2, name: 'РезинаЧел', img: 'https://sun9-50.userapi.com/impf/c855324/v855324928/70d14/AGb1kIeubsY.jpg?size=1024x1024&quality=96&sign=2d865e31b4a9532859899440f2a3ab42&type=album' },
+  { id: 3, name: 'Динамо', img: 'https://cdn.shopify.com/s/files/1/1292/7365/products/182661_1.jpg?v=1599297085' },
+  { id: 4, name: 'Моск', img: 'https://hightech.fm/wp-content/uploads/2020/12/shutterstock_1709118223.jpg' },
+  { id: 5, name: 'ЛаваЧел', img: 'https://libtime.ru/wp-content/uploads/2018/01/vulkan-kilauea.jpg' },
+  { id: 6, name: 'Торнадо', img: 'https://www.m24.ru/b/d/nBkSUhL2hFUjnci1IL6BvMKnxdDs95C-miTNnuWR9mOBdDebBizCnTY8qdJf6ReJ58vU9meMMok3Ee2nhSR6ISeO9G1N_wjJ=LRkodHIMi7MoV78b2TN4Yg.jpg' }
+];
